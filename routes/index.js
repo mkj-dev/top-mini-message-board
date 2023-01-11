630810cfe0e0810845f38e345385b471dcb0bdfb
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-01-11 17:33:29 +0100

    Add some example messages to render

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+// Example messages
+const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+ ];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express App' });
+  res.render('index', { title: "Mini message board", messages: messages });
 });
 
 module.exports = router;
